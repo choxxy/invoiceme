07661f742d2e44b79aaf51de54b471e8eba12a65
Joshua Achoka <jorh.choxxy@gmail.com>
2023-04-13 10:08:09 +0300

    Updates

---
+++ libs/form-builder/build/generated/source/buildConfig/debug/com/dsc/form_builder/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dsc.form_builder;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.dsc.form_builder";
+  public static final String BUILD_TYPE = "debug";
+}
